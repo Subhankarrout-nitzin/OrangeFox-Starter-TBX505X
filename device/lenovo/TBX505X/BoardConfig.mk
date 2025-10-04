883ed281c3379e9d2c447afac5d335b837ccf440
Subhankarrout-nitzin <subhankarrout687@gmail.com>
2025-10-04 09:50:42 +0530

    Create BoardConfig.mk

---
+++ device/lenovo/TBX505X/BoardConfig.mk
@@ -0,0 +1,14 @@
+# Board Config for TB-X505X
+
+# Device info
+TARGET_BOARD := TBX505X
+TARGET_DEVICE := TBX505X
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+
+# Recovery
+BOARD_RECOVERYIMAGE_NAME := recovery.img
+TARGET_RECOVERY_FSTAB := device/lenovo/TBX505X/recovery/root/fstab.TBX505X
+
+# Kernel (tum later add karoge prebuilt/ me)
+TARGET_PREBUILT_KERNEL := device/lenovo/TBX505X/prebuilt/Image.gz
